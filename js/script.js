b70b4b2aa1167712644e6e35e7b3a8ac46147c05
LeXX <spololin@outlook.com>
2015-11-05 20:40:00 +0300

    +js slide form-search

--- js/script.js
+++ js/script.js
@@ -1,25 +1,26 @@
 function initialize() {
-  var mapOptions = {
-    zoom: 8,
-    center: new google.maps.LatLng(34.89043, -111.74194)
-  }
-  var map = new google.maps.Map(document.getElementById('map-canvas'),
-                                mapOptions);
-  var image = "img/balun.png";
-  var myLatLng = new google.maps.LatLng(34.89043, -111.74194);
-  var beachMarker = new google.maps.Marker({
-    position: myLatLng,
-    map: map,
-    icon: image
-  });
+    var mapOptions = {
+        zoom:8, 
+        center:new google.maps.LatLng(34.89043,-111.74194) 
+    }
+    var map=new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    var image="img/balun.png";
+    var myLatLng=new google.maps.LatLng(34.89043,-111.74194);
+    var beachMarker=new google.maps.Marker({
+        position:myLatLng,
+        map:map,
+        icon:image});
 }
-google.maps.event.addDomListener(window, 'load', initialize);
+google.maps.event.addDomListener(window,'load',initialize);
 
 $(document).ready(function(){
-    function tra-ta-ta(){
     $('#btn-search').click(function(evt) {
         evt.preventDefault();
-        $('#search-form').toggleClass('hidden');
-    });
+//        $('#search-form').toggleClass('hidden');
+        if ($('#search-form').is(':hidden')) {
+  		    $('#search-form').slideDown(300);
+        } else {
+            $('#search-form').slideUp(300);
         }
+    });
 });
