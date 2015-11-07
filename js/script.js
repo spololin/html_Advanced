140b59f646655fd1ee419a4b1a13f01d007bac81
LeXX <spololin@outlook.com>
2015-11-07 11:31:48 +0300

    border-color valid

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ function initialize() {
     var mapOptions = {
         zoom:8, 
         center:new google.maps.LatLng(34.89043,-111.74194) 
-    }
+    };
     var map=new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
     var image="img/balun.png";
     var myLatLng=new google.maps.LatLng(34.89043,-111.74194);
@@ -13,20 +13,26 @@ function initialize() {
 }
 google.maps.event.addDomListener(window,'load',initialize);
 
-$(document).ready(function(){
-    $('#btn-search').click(function(evt) {
-        evt.preventDefault();
-        $('#search-form').removeClass('hidden');
-        $('#search-form').toggleClass('popup-show');
-        $('#search-form').toggleClass('popup-hide');
+
+$('.btn-search').click(function(evt) {
+    evt.preventDefault();
+    $('.search-hotel-form').removeClass('hidden');
+    $('.search-hotel-form').toggleClass('form-show');
+    $('.search-hotel-form').toggleClass('form-hide');
+}); 
+
+$('.search-hotel-form').submit(function() {
+    $('.form-input').each(function() {
+        checkInputValue($(this));
     }); 
-    
-    $('#search-form').submit(function() {
-        $('.form-input').each(function() {
-            if ($(this).val()=='') {
-                $(this).css('border', '2px solid red');
-            }
-        })
-    })
 });
 
+$('.form-input').change(function(){
+    $(this).css('border-color', '#f2f2f2');
+});
+
+function checkInputValue(obj){
+    if (obj.val()=='') {
+        obj.css('border-color', 'red');
+    };
+};
